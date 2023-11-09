8a3505cf8187e66b391a2d89e89ee757f84eed13
Ana-Laura-Moratelli <analauramoratelli203@gmail.com>
2023-11-09 15:30:27 -0300

    criei a lista no app.py dos projetos

--- app/app.py
+++ app/app.py
@@ -14,9 +14,31 @@ def sobremim():
 @app.route('/projetos')
 def projetos():
     projetos = [
-        ('Portfólio', 'Meu portfólio que foi desenvolvido com o objetivo de ter as minhas informações pessoais, acadêmicas e meus contatos.','Acesse o repositório'),
+        {
+            'img': '../static/img/img_api.png',
+            'titulo': 'Nefrologia Pediátrica',
+            'texto': 'Site desenvolvido para auxiliar mães com filhos que sofrem de doença renal crônica e notícias importantes sobre Nefrologia Pediátrica.',
+            'link': 'https://github.com/Sync-FATEC/API-NEFRO',
+            'acesse': 'Acesse o repositório',
+        },
+        {
+            'img': '../static/img/img_site_unes.png',
+            'titulo': 'Universidade Unes',
+            'texto': 'O site simula informações acadêmicas,desenvolvido para praticar para minhas habilidades de desenvolvimento web.',
+            'link': 'https://github.com/Ana-Laura-Moratelli/site-unes',
+            'acesse': 'Acesse o repositório',
+        },
+         {
+            'img': '../static/img/img_port.png',
+            'titulo': 'Portfólio',
+            'texto': 'Meu portfólio que foi desenvolvido com o objetivo de ter as minhas informações pessoais, acadêmicas e meus contatos.',
+            'link': 'https://github.com/Ana-Laura-Moratelli',
+            'acesse': 'Acesse o repositório',
+        },
+        # Adicione mais projetos aqui
     ]
-    return render_template('projetos.html',  projetos=projetos)
+    return render_template('projetos.html', projetos=projetos)
+
 
 @app.route('/contatos')
 def contatos():
